1ac5846efc4e0d04e67387103285b485162b0ede
David Hsu <dvdhsu@gmail.com>
2014-10-26 00:27:58 +0100

    Add Swaps controller and views.

---
+++ app/controllers/swaps_controller.rb
@@ -0,0 +1,8 @@
+class SwapsController < ApplicationController
+  before_action :authenticate_user!, only: [
+    :index
+  ]
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Proofswap::Application.routes.draw do
+  get 'swaps/index'
+
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
   get "inside", to: "pages#inside", as: "inside"
   
-    
   devise_for :users
+
+  resources :swaps
   
   namespace :admin do
     root "base#index"
