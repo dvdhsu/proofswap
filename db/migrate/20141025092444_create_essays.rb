a0261c8e2aab7b224596092c5245b9d2f55b6d59
David Hsu <dvdhsu@gmail.com>
2014-10-26 23:01:35 +0000

    Change column name on Essay model.

--- db/migrate/20141025092444_create_essays.rb
+++ db/migrate/20141025092444_create_essays.rb
@@ -1,7 +1,7 @@
 class CreateEssays < ActiveRecord::Migration
   def change
     create_table :essays do |t|
-      t.string :link
+      t.string :filepicker_url
       t.references :swap, index: true
       t.references :user, index: true
       t.string :name
