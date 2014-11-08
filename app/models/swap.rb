197f9b69fa22430d3f694d76edba00019a7d71e6
David Hsu <dvdhsu@gmail.com>
2014-11-08 15:53:44 +0000

    Change `has_many' relationship name from `essay' to `paper' in Swap, following previous renaming.

--- app/models/swap.rb
+++ app/models/swap.rb
@@ -1,4 +1,27 @@
 class Swap < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
   has_and_belongs_to_many :users
-  has_many :essays
+  has_many :papers
+
+  def time_left
+    self.expires.past? ? "Expired" : time_ago_in_words(self.expires, include_seconds: true)
+  end
+
+  def other_papers(self_id)
+    self.papers.select { |paper| paper.user.id != self_id }
+  end
+
+  def own_papers(self_id)
+    self.papers.select { |paper| paper.user.id == self_id }
+  end
+
+  def other_user(self_id)
+    for user in self.users
+      if user.id != self_id
+        return user
+      end
+    end
+    nil
+  end
+
 end
