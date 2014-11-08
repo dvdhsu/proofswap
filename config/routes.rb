a57a56c1f9db761a51244977de20cd779bb7800d
David Hsu <dvdhsu@gmail.com>
2014-11-08 16:22:52 +0000

    Remove un-needed route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Proofswap::Application.routes.draw do
 
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
-  get "inside", to: "pages#inside", as: "inside"
   
   devise_for :users
 
