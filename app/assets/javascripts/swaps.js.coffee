1a77a5bd9ea8f4f0b787e8f0f793a405af4982ba
David Hsu <dvdhsu@gmail.com>
2014-11-08 16:22:55 +0000

    Add interfaces for swaps.
    This commit adds an interface for swapping papers. Swaps are done via
    Filepicker.
    
    Todos:
      * make pending swaps prettier
      * add conditions on papers / swaps (e.g. requiring titles)
      * adding beter conditions for matching swaps
      * editting swaps

--- app/assets/javascripts/swaps.js.coffee
+++ app/assets/javascripts/swaps.js.coffee
@@ -1,3 +1,34 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$ ->
+  filepicker.setKey("Ans6MgPN6QUmtmP6WJBVmz")
+
+  store = (urlField, titleField, callback) ->
+    filepicker.pick({
+        mimetypes: ['text/*', 'application/msword', 'application/pdf',
+                    'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
+        services: ['COMPUTER', 'BOX', 'DROPBOX', 'GMAIL', 'GOOGLE_DRIVE', 'URL'],
+        openTo: 'COMPUTER'
+      },
+      (Blob) ->
+        full_filename = Blob.filename
+        # remove filename extension
+        index = full_filename.lastIndexOf('.')
+        filename = full_filename.substring(0, index)
+
+        urlField.val(Blob.url)
+        titleField.val(filename)
+        callback();
+    )
+
+  $('.addPaperButton').click ->
+    # numerical id (e.g. `3')
+    id = this.id.substr(this.id.length - 1)
+    store($('#addPaperForm' + id).children("input#add_paper_filepicker_url"),
+          $('#addPaperForm' + id).children("input#add_paper_title"),
+          () -> $('#addPaperForm' + id).submit())
+
+  $('#uploadButton').click ->
+    store($('#new_swap_filepicker_url'), $('#new_swap_title'), $.noop)
--- app/controllers/swaps_controller.rb
+++ app/controllers/swaps_controller.rb
@@ -1,6 +1,70 @@
 class SwapsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [
+    :index
+  ]
 
   def index
+    @paired_swaps, @unpaired_swaps = current_user.swaps.partition { |swap| swap.users.count > 1 }
+  end
+
+  def create
+    puts params
+    puts params[:new_swap_title]
+    puts params[:new_swap_filepicker_url]
+    puts params[:new_swap_expires]
+
+    unpaired_swaps = Swap.where(users_count: 1)
+    # hacky, but is better than a string condition...
+    avail_swaps = unpaired_swaps.to_a.select { |swap| swap.user_ids.exclude? current_user.id }
+
+    expires = DateTime.now
+
+    case params[:new_swap_expires]
+    when "24 hours"
+      expires = expires.advance(hours: 24)
+    else
+      expires = expires.advance(days: params[:new_swap_expires].to_s.to_i)
+    end
+
+    if avail_swaps.length > 0
+      puts "Found a swap."
+      swap = avail_swaps.first
+      swap.users << current_user
+      swap.users_count = swap.users_count + 1
+
+      # pick the earlier expiry date
+      swap.expires = expires < swap.expires ? expires : swap.expires
+
+      swap.save!
+
+      swap.papers.create!(
+        filepicker_url: params[:new_swap_filepicker_url],
+        title: params[:new_swap_title],
+        user_id: current_user.id
+      )
+    else
+      puts "Can't find a swap, so making new one."
+
+      swap = current_user.swaps.create!(
+        expires: expires,
+        users_count: 1
+      )
+
+      swap.papers.create!(
+        filepicker_url: params[:new_swap_filepicker_url],
+        title: params[:new_swap_title],
+        user_id: current_user.id
+      )
+    end
+    redirect_to swaps_path
+  end
+
+  def update
+    current_user.swaps.find(params[:id]).papers.create!(
+      filepicker_url: params[:add_paper_filepicker_url],
+      title: params[:add_paper_title],
+      user_id: current_user.id
+    )
+    redirect_to swaps_path
   end
 end
