4d96bb9e33e28b73111abb6fdc34866148a728ad
David Hsu <dvdhsu@gmail.com>
2014-11-08 20:46:27 +0000

    Fix bug with permitted parameters.
    After `college', `first_name', and `last_name' fields were added to
    user, the parameters were not permitted during controller sanitization.
    This allows them, which fixes sign-up and account editting issues.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,17 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
       :email, 
+      :first_name,
+      :last_name,
+      :college,
       :password, 
       :password_confirmation) 
     }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
       :email, 
+      :first_name,
+      :last_name,
+      :college,
       :password, 
       :password_confirmation,
       :current_password
